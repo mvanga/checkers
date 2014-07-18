22c920921fef6d72e3e3ae6f148602a7610eaca6
Felipe Cerqueira <felipec@mpi-sws.org>
2014-07-18 15:46:50 +0200

    Tests

---
+++ tests/prod-cons-bug.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_PROD 5
+#define NUM_CONS 5
+#define MAXSIZE 1000
+#define TRIALS 100
+
+int buffer[MAXSIZE];
+int size;
+int error;
+
+pthread_mutex_t lock;
+pthread_t threads[NUM_PROD + NUM_CONS];
+
+void * produce (void *arg) {
+	int i;
+	for(i = 0; i < TRIALS; i++) {
+		pthread_mutex_lock(&lock);
+		//buffer[size] = 1;
+		size++;
+		pthread_mutex_unlock(&lock);
+	}
+	return NULL;
+}
+
+void * consume (void *arg) {
+	int i;
+	for(i = 0; i < TRIALS; i++) {
+		pthread_mutex_lock(&lock);
+		//buffer[size-1]++;
+		size--;
+		pthread_mutex_unlock(&lock);
+	}
+	return NULL;
+}
+
+void setup() {
+	int i;
+	size = 0;
+
+	for (i = 0; i < NUM_PROD; i++)
+		pthread_create(&threads[i], NULL, produce, NULL);
+
+	for (i = 0; i < NUM_CONS; i++)
+		pthread_create(&threads[i+NUM_PROD], NULL, consume, NULL);
+}
+
+int run() {
+	int i;
+	for (i = 0; i < NUM_PROD+NUM_CONS; i++)
+		pthread_join(threads[i], NULL);	
+	
+	return (size >= 0 && size <= MAXSIZE);
+}
+
+int main() {
+	setup();
+	int ret = run();
+	printf("OK: %d\n", ret);
+	
+	return 0;
+}
