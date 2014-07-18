dbfe76009c2023f7629840d19a964f8e5106d099
Manohar Vanga <mvanga@mpi-sws.org>
2014-07-18 15:45:29 +0200

    Override all functions correctly
    Signed-off-by: Manohar Vanga <mvanga@mpi-sws.org>

--- preload/chess.c
+++ preload/chess.c
@@ -1,6 +1,54 @@
-#include <stdio.h>
+#define _GNU_SOURCE
 
-FILE *fopen(const char *path, const char *mode) {
-	printf("Always failing fopen\n");
+#include <pthread.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+void *my_blocker(void *arg)
+{
+	void *(*start_routine)(void *);
+	start_routine = arg;
+	printf("whee\n");
+	start_routine(NULL);
 	return NULL;
+}
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg)
+{
+	printf("Creating pthread\n");
+	fflush(0);
+
+	int (*original)(pthread_t *, const pthread_attr_t *, void *(*start_routine)(void*), void *);
+	original = dlsym(RTLD_NEXT, "pthread_create");
+	return (*original)(thread, attr, my_blocker, start_routine);
+}
+
+int pthread_mutex_lock(pthread_mutex_t *t)
+{
+	printf("Locking in pthread\n");
+	fflush(0);
+
+	int (*original)(pthread_mutex_t *);
+	original = dlsym(RTLD_NEXT, "pthread_mutex_lock");
+	return (*original)(t);
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *t)
+{
+	printf("Unlocking in pthread\n");
+	fflush(0);
+
+	int (*original)(pthread_mutex_t *);
+	original = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
+	return (*original)(t);
+}
+
+int pthread_join(pthread_t p, void **ret)
+{
+	printf("Joining pthread\n");
+	fflush(0);
+
+	int (*original)(pthread_t, void **);
+	original = dlsym(RTLD_NEXT, "pthread_join");
+	return (*original)(p, ret);
 }
--- preload/test.c
+++ preload/test.c
@@ -1,19 +1,77 @@
-#include <stdio.h>
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	FILE *fd;
+//
+//	printf("Calling fopen(...\n");
+//
+//	fd = fopen("test.txt", "w+");
+//	if (!fd) {
+//		printf("fopen() returned NULL\n");
+//		return 1;
+//	}
+//
+//	printf("fopen() succeeded\n");
+//	fclose(fd);
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define MAX 5
+
+pthread_t tid[MAX];
+int counter;
+pthread_mutex_t lock;
+
+void* doSomeThing(void *arg)
+{
+	pthread_mutex_lock(&lock);
+
+	unsigned long i = 0;
+	counter += 1;
+	printf("\n Job %d started\n", counter);
+	fflush(0);
+
+	for(i=0; i<(100000000);i++);
+
+	printf("\n Job %d finished\n", counter);
+	fflush(0);
+
+	pthread_mutex_unlock(&lock);
+
+	return NULL;
+}
 
 int main(void)
 {
-	FILE *fd;
+	int i = 0;
+	int err;
 
-	printf("Calling fopen(...\n");
-
-	fd = fopen("test.txt", "w+");
-	if (!fd) {
-		printf("fopen() returned NULL\n");
+	if (pthread_mutex_init(&lock, NULL) != 0)
+	{
+		printf("\n mutex init failed\n");
 		return 1;
 	}
 
-	printf("fopen() succeeded\n");
-	fclose(fd);
+	while(i < MAX) {
+		err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
+		if (err != 0)
+			printf("\ncan't create thread :[%s]", strerror(err));
+		i++;
+	}
+
+	i = 0;
+	while (i < MAX)
+		pthread_join(tid[i++], NULL);
+
+	pthread_mutex_destroy(&lock);
 
 	return 0;
 }
