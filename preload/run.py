4ae8e29dd69b970d9ec62a9dfb83ea2072c04b0b
Felipe Cerqueira <felipec@mpi-sws.org>
2014-07-20 19:48:17 +0200

    fix python

--- preload/run.py
+++ preload/run.py
@@ -1,12 +1,23 @@
 import sys
 import subprocess
+import itertools
 
 def main():
     # parse command line options
     if len(sys.argv) == 2:
         prog = sys.argv[1]
-        subprocess.call("MODE=record LD_PRELOAD=./chess.so " + prog, shell=True)
-    
-    
+        p = subprocess.Popen("MODE=record FILE=schedule.txt LD_PRELOAD=./chess.so " + prog, shell=True)
+        p.wait()
+        
+        with open('schedule.txt') as f:
+            l = f.read().split()
+
+        for perm in itertools.permutations(l):
+            print perm
+            with open('schedule2.txt', 'w') as f:
+                f.write("\n".join(perm))
+                p = subprocess.Popen("MODE=replay FILE=schedule2.txt LD_PRELOAD=./chess.so " + prog, shell=True)
+                p.wait()
+
 if __name__ == "__main__":
     main()
