795c6ad970bef9e7e093d804fab465709c6a13bb
Manohar Vanga <mvanga@mpi-sws.org>
2014-07-21 15:21:53 +0200

    Fix run.py
    Signed-off-by: Manohar Vanga <mvanga@mpi-sws.org>

--- preload/run.py
+++ preload/run.py
@@ -6,18 +6,21 @@ def main():
     # parse command line options
     if len(sys.argv) == 2:
         prog = sys.argv[1]
-        p = subprocess.Popen("MODE=record FILE=schedule.txt LD_PRELOAD=./chess.so " + prog, shell=True)
+        record_cmd = "MODE=record FILE=schedule.txt LD_PRELOAD=./chess.so " + prog
+        replay_cmd = "MODE=replay FILE=schedule2.txt LD_PRELOAD=./chess.so " + prog
+
+        p = subprocess.Popen(record_cmd, shell=True)
         p.wait()
         
         with open('schedule.txt') as f:
-            l = f.read().split()
+            l = [a.split()[0] for a in f.read().strip().split('\n')]
 
         for perm in itertools.permutations(l):
             print perm
             with open('schedule2.txt', 'w') as f:
                 f.write("\n".join(perm))
-                p = subprocess.Popen("MODE=replay FILE=schedule2.txt LD_PRELOAD=./chess.so " + prog, shell=True)
-                p.wait()
+            p = subprocess.Popen(replay_cmd, shell=True)
+            p.wait()
 
 if __name__ == "__main__":
     main()
