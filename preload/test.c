4baf823bd4edacacf0cdd3d9f73bbe68ef897231
Manohar Vanga <mvanga@mpi-sws.org>
2014-07-21 15:30:04 +0200

    Cleanup test.c
    Signed-off-by: Manohar Vanga <mvanga@mpi-sws.org>

--- preload/test.c
+++ preload/test.c
@@ -1,23 +1,3 @@
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	FILE *fd;
-//
-//	printf("Calling fopen(...\n");
-//
-//	fd = fopen("test.txt", "w+");
-//	if (!fd) {
-//		printf("fopen() returned NULL\n");
-//		return 1;
-//	}
-//
-//	printf("fopen() succeeded\n");
-//	fclose(fd);
-//
-//	return 0;
-//}
-
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
@@ -27,10 +7,11 @@
 #define MAX 2
 
 pthread_t tid[MAX];
+
 int counter;
 pthread_mutex_t lock;
 
-void* doSomeThing(void *arg)
+void* worker(void *arg)
 {
 	printf("\tBefore lock\n");
 	fflush(0);
@@ -46,8 +27,8 @@ void* doSomeThing(void *arg)
 
 	printf("\tAfter lock\n");
 	fflush(0);
-	
-	return NULL;
+
+	return arg;
 }
 
 int main(void)
@@ -55,14 +36,13 @@ int main(void)
 	int i = 0;
 	int err;
 
-	if (pthread_mutex_init(&lock, NULL) != 0)
-	{
+	if (pthread_mutex_init(&lock, NULL) != 0) {
 		printf("\n mutex init failed\n");
 		return 1;
 	}
 
 	while(i < MAX) {
-		err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
+		err = pthread_create(&(tid[i]), NULL, &worker, NULL);
 		if (err != 0)
 			printf("\ncan't create thread :[%s]", strerror(err));
 		i++;
